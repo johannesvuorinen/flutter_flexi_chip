e4293763f7cbcee82ed11deeba3fdfa5f80254ec
Irfan Vigma Taufik <davigmacode@gmail.com>
2022-09-30 13:18:07 +0700

    Update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:flexi_chip/flexi_chip.dart';
 import 'package:flutter/material.dart';
+import 'package:flexi_chip/flexi_chip.dart';
+import 'package:theme_patrol/theme_patrol.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,36 +12,40 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return ThemePatrol(
+      light: ThemeData(
+        brightness: Brightness.light,
+        colorScheme: ColorScheme.fromSeed(
+          brightness: Brightness.light,
+          seedColor: Colors.red,
+        ),
+        materialTapTargetSize: MaterialTapTargetSize.padded,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      dark: ThemeData(
+        brightness: Brightness.dark,
+        colorScheme: ColorScheme.fromSeed(
+          brightness: Brightness.dark,
+          seedColor: Colors.red,
+        ),
+        toggleableActiveColor: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        materialTapTargetSize: MaterialTapTargetSize.padded,
+      ),
+      builder: (context, theme) {
+        return MaterialApp(
+          title: 'FlexiChip',
+          theme: theme.lightData,
+          darkTheme: theme.darkData,
+          themeMode: theme.mode,
+          home: const MyHomePage(title: 'FlexiChip Example'),
+        );
+      },
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -56,33 +61,60 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        actions: <Widget>[
+          ThemeConsumer(builder: ((context, theme) {
+            return IconButton(
+              onPressed: () => theme.toggleMode(),
+              icon: Icon(theme.modeIcon),
+            );
+          })),
+        ],
       ),
       body: Center(
         child: Wrap(
           spacing: 5,
           children: [
             FlexiChip(
-              style: const FlexiChipStyle.outlined(),
-              activeStyle: const FlexiChipStyle.elevated(
-                useCheckmark: true,
-                color: Colors.green,
-              ),
+              style: FlexiChipStyle.stated((states) {
+                if (states.contains(MaterialState.selected)) {
+                  return const FlexiChipStyle.elevated(
+                    useCheckmark: true,
+                    color: Colors.green,
+                  );
+                }
+                return const FlexiChipStyle.outlined(
+                  color: Colors.green,
+                  borderWidth: 2,
+                );
+              }),
+              // style: const FlexiChipStyle.outlined(
+              //   color: Colors.green,
+              //   borderWidth: 2,
+              // ),
+              // activeStyle: const FlexiChipStyle.elevated(
+              //   useCheckmark: true,
+              //   color: Colors.green,
+              // ),
               label: const Text('Dart'),
               // trailing: const Icon(Icons.close),
+              // disabled: true,
               selected: _selected,
               onSelected: (selected) => setState(() => _selected = selected),
+              onDeleted: () => print('delete'),
             ),
             FlexiChip(
               // style: const FlexiChipStyle.outlined(),
-              activeStyle: const FlexiChipStyle.outlined(
-                // useCheckmark: true,
+              selectedStyle: const FlexiChipStyle.outlined(
+                useCheckmark: true,
                 color: Colors.red,
                 borderWidth: 2,
                 borderRadius: BorderRadius.all(Radius.circular(15)),
                 foregroundStyle: TextStyle(fontWeight: FontWeight.bold),
               ),
               label: const Text('TS'),
-              // trailing: const Icon(Icons.close),
+              trailing: const Icon(Icons.close),
+              tooltip: 'Typescript',
+              // disabled: true,
               selected: _selected,
               onSelected: (selected) => setState(() => _selected = selected),
             ),
